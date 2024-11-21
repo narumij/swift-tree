79267763d10324da391c09a1fbc1c97861e05ad9
narumij <narumij@gmail.com>
2024-11-21 21:43:15 +0900

    package name

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-tree",
+    name: "AcCollections",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "tree",
+            name: "AcCollections",
             targets: ["tree"]),
     ],
     targets: [
